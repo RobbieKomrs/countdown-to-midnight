9c7a651fbfea974a35da78848ee05d1ba947c031
RobbieKomrs <robbiekomrs@gmail.com>
2020-12-17 16:05:23 -0800

    commit tree 5cbfd082480cfa9e9788425ebd884f4561b8b33e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+def countdown(clock)
+while clock >= 1
+  puts "#{clock} SECOND(S)!"
+    clock -= 1
+    
+end
 
-def countdown
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(clock)
+while clock >= 1
+  puts "#{clock} SECOND(S)!"
+  sleep(1)
+    clock -= 1
+    
+    
+end
 end
